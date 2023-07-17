061ce9f2a1e5559a4c6088da3274ea58355b64e6
Zoltan Palinkaš <zoltan.palinkas1@gmail.com>
2023-07-17 11:47:58 +0200

    di register fix

--- InternHub/InternHub.WebApi/App_Start/Startup.Auth.cs
+++ InternHub/InternHub.WebApi/App_Start/Startup.Auth.cs
@@ -26,8 +26,6 @@ namespace InternHub.WebApi
         // For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
-            DependencyInjectionConfig.Register();
-
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
             app.UseCookieAuthentication(new CookieAuthenticationOptions());
